b92ed6bcbb88f1a378862ab8057bf94539bfe34f
arijitdeb1502 <arijitdeb.git@gmail.com>
2020-03-15 07:58:39 +0800

    Adding the first version of the social media chat app frontend project

---
+++ src/ChattingApp.js
@@ -0,0 +1,18 @@
+import React,{Component} from 'react';
+import './ChattingApp.css';
+
+class ChattingApp extends Component{
+
+    render() {
+      return(
+          <div className="App">
+            ChattingApp Frontend
+          </div>
+      )
+    }
+
+
+}
+
+
+export default ChattingApp;
