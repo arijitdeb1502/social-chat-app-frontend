04372f36bbadc7048b9a8a2a3fdb8c6a91ca202f
arijitdeb1502 <arijitdeb.git@gmail.com>
2020-03-18 07:03:51 +0800

    Changing the Header to stateless functional component to class component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,35 +1,44 @@
-import React from 'react';
+import React,{Component} from 'react';
 import { NavLink } from 'react-router-dom';
 import './styles/Header.css';
 
-const Header = ({heading})=> ( 
-    <div className="header-element">
-        <div className="app-name">
-            {heading}
-        </div>
-        <div className="all-pages">
-            <NavLink to="/downloads" exact={true} className="nav-link" activeClassName="is-active">
-                downloads    
-            </NavLink>
-            <NavLink to="/help" exact={true} className="nav-link" activeClassName="is-active">
-                help    
-            </NavLink>
-            <NavLink to="/features" exact={true} className="nav-link" activeClassName="is-active">
-                Features
-            </NavLink>
-        </div>
-        <div className="user-registration-or-signin-menu">
-            <button className="signin-button">Sign in</button>
-            <div className="dropdown-menu">
-                <NavLink to="/registration" exact={true} className="nav-link" activeClassName="is-active">
-                    Registration
+
+class Header extends Component {
+
+    render(){
+
+        return(
+          <div className="header-element">
+            <div className="app-name">
+                {this.props.heading}
+            </div>
+            <div className="all-pages">
+                <NavLink to="/downloads" exact={true} className="nav-link" activeClassName="is-active">
+                    downloads    
                 </NavLink>
-                <NavLink to="/signin" exact={true} className="nav-link" activeClassName="is-active">
-                    Sign in
+                <NavLink to="/help" exact={true} className="nav-link" activeClassName="is-active">
+                    help    
+                </NavLink>
+                <NavLink to="/features" exact={true} className="nav-link" activeClassName="is-active">
+                    Features
                 </NavLink>
             </div>
+            <div className="user-registration-or-signin-menu">
+                <button className="signin-button">Sign in</button>
+                <div className="dropdown-menu" id="dropdown-item-id">
+                    <NavLink to="/registration" exact={true} className="nav-link" activeClassName="is-active">
+                        Registration
+                    </NavLink>
+                    <NavLink to="/signin" exact={true} className="nav-link" activeClassName="is-active">
+                        Sign in
+                    </NavLink>
+                </div>
+            </div>
         </div>
-    </div>
-)
+        )
+    }
+    
+}
+
 
 export default Header;
