ea22600b6bce3a91e7255955ae32c7a3f3499fc3
arijitdeb1502 <arijitdeb.git@gmail.com>
2020-03-19 07:18:51 +0800

    Removing un-used variables

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,50 +1,6 @@
-import React,{Component} from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './styles/Header.css';
-
-
-// class Header extends Component {
-
-//     constructor(props){
-//         super(props);
-//     }
-
-//     render(){    
-
-//         return(
-//           <div className="header-element">
-//             <div className="app-name">
-//                 {this.props.heading}
-//             </div>
-//             <div className="all-pages">
-//                 <NavLink to="/downloads" exact={true} className="nav-link" activeClassName="is-active">
-//                     downloads    
-//                 </NavLink>
-//                 <NavLink to="/help" exact={true} className="nav-link" activeClassName="is-active">
-//                     help    
-//                 </NavLink>
-//                 <NavLink to="/features" exact={true} className="nav-link" activeClassName="is-active">
-//                     Features
-//                 </NavLink>
-//             </div>
-//             <div className="user-registration-or-signin-menu">
-//                 <button className="signin-button">Sign in</button>
-//                 <div className="dropdown-menu" id="dropdown-item-id">
-//                     <NavLink to="/registration" exact={true} className="nav-link-dropdown" activeClassName="is-active">
-//                         Registration
-//                     </NavLink>
-//                     <NavLink to="/signin" exact={true} className="nav-link-dropdown" activeClassName="is-active">
-//                         Sign in
-//                     </NavLink>
-//                 </div>
-//             </div>
-//         </div>
-//         )
-//     }
-    
-// }
-
-
 
 const handleDropdown=()=>{
     document.getElementById("dropdown-item-id").classList.toggle("show");
