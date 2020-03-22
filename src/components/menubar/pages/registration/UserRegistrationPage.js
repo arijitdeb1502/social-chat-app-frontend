94c5537cd21c4a75ce33f8cd568b5f3209bdb589
arijitdeb1502 <arijitdeb.git@gmail.com>
2020-03-22 09:02:28 +0800

    Started Adding the Registration page eventHandlers

--- src/components/menubar/pages/registration/UserRegistrationPage.js
+++ src/components/menubar/pages/registration/UserRegistrationPage.js
@@ -7,32 +7,86 @@ class UserRegistration extends Component {
     constructor(props){
 
         super(props);
-        this.onClickHandler=this.onClickHandler.bind(this);
+
+        this.onBackButtonClickHandler=this.onBackButtonClickHandler.bind(this);
+        this.onFormSubmitHandler=this.onFormSubmitHandler.bind(this);
+        this.onChangeHandler=this.onChangeHandler.bind(this);
+
+        this.state={
+          name: '',
+          email: '',
+          password: '',
+          age: ''
+        }
 
     }
 
-    onClickHandler(){
+
+    onChangeHandler = (e)=>{
+
+      // console.log(this.state);
+      this.setState( { [e.target.name] : e.target.value } );
+
+      // this.setState( ()=>({ 
+      //    [e.persist().target.name] : e.target.value 
+      // })
+      // );
+
+    }
+
+
+    onBackButtonClickHandler = ()=>{
         this.props.resetHeaderDisplayHandler();  
         //this.props.history.push("http://localhost:3000/"); 
+    }
+
+    onFormSubmitHandler = (e) => {
+        e.preventDefault();
+        console.log(e.target);
+        console.log(this.state);
     }
 
     render() {
         return (
           <div className="formContainer">
-            <form className="registrationForm">
+            <form className="registrationForm" onSubmit={this.onFormSubmitHandler}>
               <h1 className="formHeader">Create Account</h1>  
               <label>Name: </label>
-              <input type="text"></input>
+              <input 
+                type="text"
+                name="name"
+                placeholder="name"
+                value={this.state.name}
+                onChange={this.onChangeHandler}      
+              />
               <label>Email: </label>
-              <input type="text"></input>
+              <input 
+                type="email"
+                name="email"
+                placeholder="email"
+                value={this.state.email}
+                onChange={this.onChangeHandler}
+              />
               <label>password: </label>
-              <input type="text"></input>
+              <input 
+                type="password"
+                name="password"
+                placeholder="password"
+                value={this.state.password}
+                onChange={this.onChangeHandler}
+              />
               <label>Age: </label>
-              <input type="text"></input>
+              <input 
+                type="number"
+                name="age"
+                placeholder="age"
+                value={this.state.age}
+                onChange={this.onChangeHandler}
+              />
               <button className="RegisterUserButton">Register User</button>
               </form>
               <NavLink to="/" exact={true} activeClassName="is-active">
-                 <button className="BackButton" onClick={this.onClickHandler}>
+                 <button className="BackButton" onClick={this.onBackButtonClickHandler}>
                      Back
                  </button>
              </NavLink> 
