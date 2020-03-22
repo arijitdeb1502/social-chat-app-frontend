7ccd0568a5670b8d8811ffe9094b4dd2fa7992c9
arijitdeb1502 <arijitdeb.git@gmail.com>
2020-03-22 15:00:30 +0800

    Adding Validators for the User Registration form

--- src/components/menubar/pages/registration/UserRegistrationPage.js
+++ src/components/menubar/pages/registration/UserRegistrationPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import validator from 'validator';
 import './styles/Registration.css';
 
 class UserRegistration extends Component {
@@ -16,7 +17,11 @@ class UserRegistration extends Component {
           name: '',
           email: '',
           password: '',
-          age: ''
+          age: '',
+          showNameErrMsg: false,
+          showEmailErrMsg: false,
+          showPasswordErrMsg: false,
+          showAgeErrMsg: false,
         }
 
     }
@@ -24,13 +29,7 @@ class UserRegistration extends Component {
 
     onChangeHandler = (e)=>{
 
-      // console.log(this.state);
       this.setState( { [e.target.name] : e.target.value } );
-
-      // this.setState( ()=>({ 
-      //    [e.persist().target.name] : e.target.value 
-      // })
-      // );
 
     }
 
@@ -42,7 +41,30 @@ class UserRegistration extends Component {
 
     onFormSubmitHandler = (e) => {
         e.preventDefault();
+        
+        const { name,email,password,age } = { ...this.state };
+        const passwordIsValid=this.isValidPassword(password);
+
+        this.setState(()=>{
+          return {
+            showNameErrMsg: !validator.isAlpha(name.replace(/ /g,'')),
+            showEmailErrMsg: !validator.isEmail(email),
+            showPasswordErrMsg: !passwordIsValid
+          }
+        })
+
+        console.log(name.replace(/ /g,''));
+
+        // const userBody={ name,email,password,age };
+
         console.log(this.state);
+        
+    }
+
+    isValidPassword=(password)=>{
+
+      return password.length >=6 ? true:false 
+
     }
 
     render() {
@@ -58,6 +80,7 @@ class UserRegistration extends Component {
                 value={this.state.name}
                 onChange={this.onChangeHandler}      
               />
+              {this.state.showNameErrMsg && <div>"{this.state.name}" is not a valid name.</div>}
               <label>Email: </label>
               <input 
                 type="email"
@@ -66,6 +89,7 @@ class UserRegistration extends Component {
                 value={this.state.email}
                 onChange={this.onChangeHandler}
               />
+              {this.state.showEmailErrMsg && <div>{this.state.email} is not a valid email.</div>}
               <label>password: </label>
               <input 
                 type="password"
@@ -74,6 +98,7 @@ class UserRegistration extends Component {
                 value={this.state.password}
                 onChange={this.onChangeHandler}
               />
+              {this.state.showPasswordErrMsg && <div>{this.state.password} is not a valid password.</div>}
               <label>Age: </label>
               <input 
                 type="number"
